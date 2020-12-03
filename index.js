8cd4ddef1ce8337b4e6cd113abf80d713bee7cd4
2439905184 <suchenglong018@126.com>
2020-12-03 20:40:03 +0800

    添加了搜索页面 移动样式标签内容至css文件中 修改了用户空间壁纸

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+//搜索按钮 跳转搜索界面
+function search()
+{
+  alert("具体细节待开发中，按确定键跳转页面")
+  window.open("search.html")
+}
+function upload()
+{
+  alert("请先登录")
+}
 $(document).ready(function()
 {
   $("#getRecommandJuzi").click(function()
