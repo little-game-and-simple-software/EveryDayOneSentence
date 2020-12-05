72d71b6bfdd3035e8e1134abeb9f94d4e99df267
2439905184 <suchenglong018@126.com>
2020-12-05 12:14:01 +0800

    添加了公告按钮 改用新的简单文字匹配搜索算法 废弃旧的复杂算法

--- searchCore.js
+++ searchCore.js
@@ -3,50 +3,53 @@ var keyword=""
 var result_array=[]
 $(function()
 {
-  $(".button-toysworld:eq(0)").click(function()
+  $(".button-toysworld:eq(1)").click(function()
   {
-    alert("toysworld类的按钮被按下")
+    //alert("toysworld类的按钮被按下")
     //获取搜索框的值
     keyword=$("#InputSearch").val()
     alert("待搜索的关键词"+keyword)
     //执行搜索方法 返回一个处理后的数组变量 待处理
     //待动态更新界面 append??
-    //var result_array=
-    //search_with_keyword(keyword)
-    test_key_Search()
-    //console.log("返回的结果->"+result_array)
+    var result=search_with_keyword(keyword)
+    //test_key_Search()
+    console.log("返回的结果->"+result)
   })
    // 开始写 jQuery 代码...
 
 })
-function test_key_Search()
-{
-  var array=["测试是",'垃圾']
-  array.fill("Ada")
-  console.log(array)
-}
+var array=["测试是",'垃圾']
+
 function search_with_keyword(keyword)
 {
   //文字数组
   var SentenceSet=["修正",'通用aaa','学生bbbbbbb']
-  //遍历
+  //遍历 改用简单算法
   //alert(SentenceSet.length)
   for(var i=0;i<SentenceSet.length;i++)
   {
     var currentValue=SentenceSet[i]
     console.log("当前的数组值->"+i+currentValue)
     console.log("当前被匹配的字符串长度"+currentValue.length)
-    for(var i2=0;i2<currentValue.length;i2++)
+    if(currentValue.indexOf(keyword)!=-1)
     {
-      //当前文字（从字符串分离出单个字）
-      var current_text=currentValue.charAt(i2)
-      console.log("当前分字->"+current_text)
-      if(current_text==keyword.charAt(i2))
-      {
-        result_array.push(SentenceSet[i])
-        alert(result_array)
-        i2+=1
-      }
+      alert("匹配到值")
+      result_array.push(currentValue)
     }
   }
+  return result_array
 }
+//-------遗弃嵌套for算法
+/*
+for(var i2=0;i2<currentValue.length;i2++)
+{
+  //当前文字（从字符串分离出单个字）
+  var current_text=currentValue.charAt(i2)
+  console.log("当前分字->"+current_text)
+  if(current_text==keyword.charAt(i2))
+  {
+    result_array.push(SentenceSet[i])
+    alert(result_array)
+    i2+=1
+  }
+}*/
