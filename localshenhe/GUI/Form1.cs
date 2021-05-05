856967aa375566e2fdc7cb44f650fc4575f7b1b8
lzr2006 <65013694+lzr2006@users.noreply.github.com>
2021-05-05 12:59:22 +0800

    Add files via upload

---
+++ localshenhe/GUI/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace EDOSLVS
+{
+    public partial class Form1 : Form
+    {
+
+        public string passwd;
+
+        int errorCount = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(10086);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (errorCount >= 3)
+            {
+                MessageBox.Show("禁止非法登录！","严重错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Environment.Exit(12336);
+            }
+            if (textBox1.Text == "admin" && textBox2.Text == "dyxsz")
+            {
+                MessageBox.Show("欢迎您！","登录成功",MessageBoxButtons.OK,MessageBoxIcon.Information);
+                Main ourMainForm = new Main();
+                ourMainForm.Show();
+            }
+            else
+            {
+                MessageBox.Show("用户名或密码错误！您还剩余" + (3-errorCount).ToString() + "次机会！","登录失败",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                errorCount++;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //加载密码
+            
+        }
+    }
+}
---
+++ localshenhe/GUI/Main.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Net.Http;
+using System.IO;
+
+namespace EDOSLVS
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            //先打开exe
+            System.Diagnostics.Process.Start(Application.StartupPath + "\\Python\\get_from_php.exe");
+            //读取文件获取数据
+
+            //暴力读取文件
+            StreamReader juzi = new StreamReader(Application.StartupPath + "\\Python\\get.txt");
+
+            try
+            {
+                
+                for (int i = 0; i < 100; i++)
+                {
+
+                    string aSentence = juzi.ReadLine();
+                    listBox1.Items.Add(aSentence);
+                }
+            }
+            catch 
+            {
+                Console.WriteLine("读取文件完成!");
+            }
+
+            finally
+            {
+                juzi.Close();
+            }            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string sentence = listBox1.SelectedItem.ToString();
+            StreamWriter writeSentencePass = new StreamWriter(Application.StartupPath + "\\Python\\pass.txt");
+            writeSentencePass.Write(sentence);
+            writeSentencePass.Flush();
+            writeSentencePass.Close();
+            MessageBox.Show(sentence + "已经通过！","审核系统",MessageBoxButtons.OK,MessageBoxIcon.Information);
+            listBox1.Items.Remove(sentence);
+            System.Diagnostics.Process.Start(Application.StartupPath + "\\Python\\Yes.exe");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string sentence = listBox1.SelectedItem.ToString();
+            StreamWriter writeSentencePass = new StreamWriter(Application.StartupPath + "\\Python\\die.txt");
+            writeSentencePass.Write(sentence);
+            writeSentencePass.Flush();
+            writeSentencePass.Close();
+            MessageBox.Show(sentence + "不能通过！", "审核系统", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            listBox1.Items.Remove(sentence);
+            for (int i = 0; i < 5000; i++)
+            {
+                //拖一会儿时间
+                Console.WriteLine(i.ToString());
+            }
+            System.Diagnostics.Process.Start(Application.StartupPath + "\\Python\\No.exe");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("结束审核，放松一下吧！","后会有期",MessageBoxButtons.OK,MessageBoxIcon.Information);
+            File.Delete(Application.StartupPath + "\\Python\\get.txt");
+            Environment.Exit(0);
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
